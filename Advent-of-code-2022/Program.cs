8376d084f834e4bd539551351a06c69f56ad4fa1
Llamexis <polaczek0907@gmail.com>
2022-12-02 19:39:51 +0100

    Part 1 done

--- Advent-of-code-2022/Program.cs
+++ Advent-of-code-2022/Program.cs
@@ -2,9 +2,48 @@
 {
     internal class Program
     {
+
+        static string[] calories =
+            {
+                "1000",
+                "2000",
+                "3000",
+                "",
+                "4000",
+                "",
+                "5000",
+                "6000",
+                "",
+                "7000",
+                "8000",
+                "9000",
+                "",
+                "10000"
+            };
+        static int max_calorie = int.MinValue;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            part1();
+        }
+
+        static void part1()
+        {
+            var lines = File.ReadAllLines("..\\..\\..\\input1.txt");
+            int sum = 0;
+            foreach(var line in lines)
+            {
+                if(line != string.Empty)
+                {
+                    sum += int.Parse(line);
+                }
+                else
+                {
+                    max_calorie = max_calorie < sum ? sum: max_calorie;
+                    sum = 0;
+                }
+            }
+            max_calorie = max_calorie < sum ? sum : max_calorie;
+            Console.WriteLine($"Maximum Calories: {max_calorie}");
         }
     }
 }
