3593ca4a7eaa6d13892ad3490738eec3e8fb86a6
Llamexis <polaczek0907@gmail.com>
2022-12-02 20:34:45 +0100

    Finished part 1

--- Advent-of-code-2022/Program.cs
+++ Advent-of-code-2022/Program.cs
@@ -4,7 +4,69 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            part1();
+        }
+
+        // Rock = {A,X} == 1
+        // Paper = {B,Y} == 2
+        // Scissors = {C,Z} == 3
+        // Win == 6
+        // Draw == 3
+        // lose == 0
+        static void part1()
+        {
+            var lines = File.ReadAllLines("..\\..\\..\\input1.txt");
+            int totalScore = 0;
+            foreach (var line in lines)
+            {
+                var choices = line.Split(' ');
+                if (choices[0] == "A")
+                {
+                    if (choices[1] == "X")
+                    {
+                        totalScore += 1 + 3;
+                    }
+                    else if (choices[1] == "Y")
+                    {
+                        totalScore += 2 + 6;
+                    }
+                    else
+                    {
+                        totalScore += 3 + 0;
+                    }
+                }
+                else if (choices[0] == "B")
+                {
+                    if (choices[1] == "X")
+                    {
+                        totalScore += 1 + 0;
+                    }
+                    else if (choices[1] == "Y")
+                    {
+                        totalScore += 2 + 3;
+                    }
+                    else
+                    {
+                        totalScore += 3 + 6;
+                    }
+                }
+                else if (choices[0] == "C")
+                {
+                    if (choices[1] == "X")
+                    {
+                        totalScore += 1 + 6;
+                    }
+                    else if (choices[1] == "Y")
+                    {
+                        totalScore += 2 + 0;
+                    }
+                    else
+                    {
+                        totalScore += 3 + 3;
+                    }
+                }
+            }
+            Console.WriteLine($"Total Score: {totalScore}");
         }
     }
 }
