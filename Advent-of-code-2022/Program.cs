972299117f25f0dcc886e40887ef72edecb43c29
Llamexis <polaczek0907@gmail.com>
2022-12-02 20:41:22 +0100

    Finished day 2

--- Advent-of-code-2022/Program.cs
+++ Advent-of-code-2022/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            part1();
+            part2();
         }
 
         // Rock = {A,X} == 1
@@ -63,6 +63,65 @@
                     else
                     {
                         totalScore += 3 + 3;
+                    }
+                }
+            }
+            Console.WriteLine($"Total Score: {totalScore}");
+        }
+
+        // Lose == x
+        // win == z
+        // draw == y
+        static void part2()
+        {
+            var lines = File.ReadAllLines("..\\..\\..\\input1.txt");
+            int totalScore = 0;
+            foreach (var line in lines)
+            {
+                var choices = line.Split(' ');
+                if (choices[0] == "A")
+                {
+                    if (choices[1] == "X")
+                    {
+                        totalScore += 3 + 0;
+                    }
+                    else if (choices[1] == "Y")
+                    {
+                        totalScore += 1 + 3;
+                    }
+                    else
+                    {
+                        totalScore += 2 + 6;
+                    }
+                }
+                else if (choices[0] == "B")
+                {
+                    if (choices[1] == "X")
+                    {
+                        totalScore += 1 + 0;
+                    }
+                    else if (choices[1] == "Y")
+                    {
+                        totalScore += 2 + 3;
+                    }
+                    else
+                    {
+                        totalScore += 3 + 6;
+                    }
+                }
+                else if (choices[0] == "C")
+                {
+                    if (choices[1] == "X")
+                    {
+                        totalScore += 2 + 0;
+                    }
+                    else if (choices[1] == "Y")
+                    {
+                        totalScore += 3 + 3;
+                    }
+                    else
+                    {
+                        totalScore += 1 + 6;
                     }
                 }
             }
