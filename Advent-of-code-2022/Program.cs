599cfd3422166cd49a2997ec396798d2624a135d
Llamexis <polaczek0907@gmail.com>
2022-12-03 13:30:27 +0100

    finished part 2

--- Advent-of-code-2022/Program.cs
+++ Advent-of-code-2022/Program.cs
@@ -14,7 +14,7 @@
         };
         static void Main(string[] args)
         {
-            part1();
+            part2();
         }
 
         static void part1()
@@ -25,6 +25,16 @@
                 lines.Select(x => x.ToCharArray().Chunk(x.Length / 2))
                 .Select(x => x.First().Intersect(x.Last()).First())
                 .Sum(x => {return x >  96 ? x - 96 : x - 38; })
+                );
+        }
+
+        static void part2()
+        {
+            var lines = File.ReadAllLines("..\\..\\..\\input.txt");
+            Console.WriteLine("Sum of priorities: " +
+                lines.Select(x => x.ToCharArray()).Chunk(3)
+                .Select(x=> x.Aggregate((a, n) => a.Intersect(n).ToArray()).First())
+                .Sum(x => { return x > 96 ? x - 96 : x - 38; })
                 );
         }
     }
