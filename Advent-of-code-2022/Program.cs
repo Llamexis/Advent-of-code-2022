c4447d66766d2e7c4bc48cc009a7ba4f681c6049
Llamexis <polaczek0907@gmail.com>
2022-12-03 13:19:20 +0100

    finished part 1

--- Advent-of-code-2022/Program.cs
+++ Advent-of-code-2022/Program.cs
@@ -2,9 +2,30 @@
 {
     internal class Program
     {
+
+        static string[] rucksacks =
+        {
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+            "PmmdzqPrVvPwwTWBwg",
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+            "ttgJtRGJQctTZtZT",
+            "CrZsJsPPZsGzwwsLwLmpwMDw"
+        };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            part1();
+        }
+
+        static void part1()
+        {
+            var lines = File.ReadAllLines("..\\..\\..\\input.txt");
+
+            Console.WriteLine("Sum of priorities: " +
+                lines.Select(x => x.ToCharArray().Chunk(x.Length / 2))
+                .Select(x => x.First().Intersect(x.Last()).First())
+                .Sum(x => {return x >  96 ? x - 96 : x - 38; })
+                );
         }
     }
 }
