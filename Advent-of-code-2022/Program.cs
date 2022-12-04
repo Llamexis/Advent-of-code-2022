3d385d76f63523b1d0fda870420a43322a28be37
Llamexis <polaczek0907@gmail.com>
2022-12-04 12:07:51 +0100

    .

--- Advent-of-code-2022/Program.cs
+++ Advent-of-code-2022/Program.cs
@@ -1,10 +1,1 @@
-﻿namespace Advent_of_code_2022
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿Console.WriteLine("Hello, World!");
