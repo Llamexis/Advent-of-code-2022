c352260409bfc0c22b9e85727cb50b185f23d308
Llamexis <polaczek0907@gmail.com>
2022-12-05 16:47:37 +0100

    Finished part 2 added input parse function

--- Advent-of-code-2022/Program.cs
+++ Advent-of-code-2022/Program.cs
@@ -1,26 +1,29 @@
 ﻿using System.Security.Cryptography.X509Certificates;
 
-var input = File.ReadAllLines("..\\..\\..\\input.txt");
 
 // Parsing Input
-
 List<Stack<char>> stacks = new List<Stack<char>>();
-for (int i = 0; i < 9; i++)
-    stacks.Add(new Stack<char>());
-for(int i = 7; i >= 0; i--)
-{
-    int indexOfStack = 0;
-    var line = input[i].ToCharArray();
-    for(int j = 1; j < line.Length; j += 4)
+string[] InputParse(ref List<Stack<char>> stacks)
+{ 
+    var input = File.ReadAllLines("..\\..\\..\\input.txt");
+    for (int i = 0; i < 9; i++)
+        stacks.Add(new Stack<char>());
+    for(int i = 7; i >= 0; i--)
     {
-        if (char.IsLetter(line[j]))
+        int indexOfStack = 0;
+        var line = input[i].ToCharArray();
+        for(int j = 1; j < line.Length; j += 4)
         {
-            stacks[indexOfStack].Push(line[j]);
+            if (char.IsLetter(line[j]))
+            {
+                stacks[indexOfStack].Push(line[j]);
+            }
+            indexOfStack++;
         }
-        indexOfStack++;
     }
+    return input;
 }
-
+var input = InputParse(ref stacks);
 // Part 1
 for(int i=10; i < input.Length; i++)
 {
@@ -28,12 +31,32 @@ for(int i=10; i < input.Length; i++)
     int[] ints = { int.Parse(values[1]), int.Parse(values[3]), int.Parse(values[5]) };
     for(int j =0; j < ints[0]; j++)
     {
-        char res;
-        stacks[ints[1]-1].TryPop(out res);
+        char res=stacks[ints[1]-1].Pop();
         stacks[ints[2]-1].Push(res);
     }
 }
 Console.Write("Messege Part 1: ");
 foreach(var stack in stacks)
+    Console.Write(stack.Pop());
+Console.WriteLine();
+
+stacks.Clear();
+input = InputParse(ref stacks);
+//part 2
+for (int i = 10; i < input.Length; i++)
+{
+    var values = input[i].Split(" ");
+    int[] ints = { int.Parse(values[1]), int.Parse(values[3]), int.Parse(values[5]) };
+    Stack<char> tmp = new Stack<char>();
+    for (int j = 0; j < ints[0]; j++)
+    {
+        char res = stacks[ints[1]-1].Pop();
+        tmp.Push(res);
+    }
+    foreach(var elem in tmp)
+        stacks[ints[2]-1].Push(elem);
+}
+Console.Write("Messege Part 2: ");
+foreach (var stack in stacks)
     Console.Write(stack.Pop());
 Console.WriteLine();
