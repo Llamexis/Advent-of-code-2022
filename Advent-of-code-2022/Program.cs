fb593c6998d7829f83fd6f73e992c2651f512339
Llamexis <polaczek0907@gmail.com>
2022-12-06 10:07:02 +0100

    Finished part 1

--- Advent-of-code-2022/Program.cs
+++ Advent-of-code-2022/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var input = File.ReadAllText("input.txt");
+
+// part 1
+var firstSeq = input.Chunk(4)
+    .ToArray()
+    .Select((str, index) => new { str, index })
+    .First(x=>x.str.Distinct().Count() == 4);
+Console.WriteLine(firstSeq.index * 4+1);
