53e37da1185738306dc4196ff33639dcd2faf4c5
Llamexis <polaczek0907@gmail.com>
2022-12-07 15:21:41 +0100

    Finished part 1

--- Advent-of-code-2022/Program.cs
+++ Advent-of-code-2022/Program.cs
@@ -1,6 +1,5 @@
 ﻿
-using System.Xml.Linq;
-
+//  Init
 var input = File.ReadAllLines("input.txt");
 Node root = new Node();
 root.children = new List<Node>();
@@ -9,7 +8,40 @@ root.name = "/";
 root.size = 0;
 Stack<Node> cwd = new Stack<Node>();
 cwd.Push(root);
+int sum = 0;
+
+//  Parsing Input
 ParseInput(ref root);
+
+// Counting Sizes in Tree
+CountSize(root, 100000, ref sum);
+
+//  Result of Part 1
+Console.WriteLine("Part 1: " + sum);
+
+
+
+
+//  Declarations and Definitions
+int CountSize(Node root, int searchSize, ref int sum)
+{
+    int size = 0;
+    
+    foreach(Node node in root.children)
+    {
+        if (node.type == "file")
+            size += node.size;
+        else if (node.type == "dir")
+        {
+            size += CountSize(node, searchSize, ref sum);
+        }
+    }
+
+    root.size= size;
+    if (size <= searchSize)
+        sum += size;
+    return size;
+}
 
 void ParseInput(ref Node root)
 { 
