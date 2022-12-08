640cd370d6ac584e7daff803bcbeadfae40fd8d2
Llamexis <polaczek0907@gmail.com>
2022-12-08 14:39:47 +0100

    Finished part 2

--- Advent-of-code-2022/Program.cs
+++ Advent-of-code-2022/Program.cs
@@ -8,10 +8,6 @@
     "35390"
 };*/
 
-bool[][] IsVisible= new bool[input.Length][];
-for(int i = 0; i < input.Length;i++)
-    IsVisible[i] = new bool[input.Length];
-
 int visible = 0;
 for(int i=0; i<input.Length; i++)
 {
@@ -28,13 +24,34 @@ for(int i=0; i<input.Length; i++)
 
 Console.WriteLine("Part 1: "+visible);
 
+List<int> scenicScores = new List<int>();
+for (int i = 0; i < input.Length; i++)
+{
+    for (int j = 0; j < input[i].Length; j++)
+    {
+        int left = CountScenicScore(j, i, -1, 0, input[i][j]);
+        int right = CountScenicScore(j, i, 1, 0, input[i][j]);
+        int up = CountScenicScore(j, i, 0, -1, input[i][j]);
+        int down = CountScenicScore(j, i, 0, 1, input[i][j]);
+        scenicScores.Add(left * right * up * down);
+    }
+}
+Console.WriteLine("Part 2: "+scenicScores.Max());
 bool CountVisible(int x, int y, int xd, int yd, char startValue)
 {
-    if(x == 0 || x == input.Length || x == input.Length-1
-        || y == 0 || y == input.Length || y == input.Length-1)
+    if (x == 0 || x == input.Length - 1 || y == 0 || y == input.Length - 1)
         return true;
     if (startValue <= input[y + yd][x + xd])
         return false;
-    
+
     return CountVisible(x + xd, y + yd, xd, yd, startValue);
+}
+int CountScenicScore(int x, int y, int xd, int yd, char startValue)
+{
+    if (x == 0 || x == input.Length - 1 || y == 0 || y == input.Length - 1)
+        return 0;
+    if (startValue <= input[y + yd][x + xd])
+        return 1 ;
+
+    return 1 + CountScenicScore(x + xd,y + yd,xd, yd, startValue);
 }
