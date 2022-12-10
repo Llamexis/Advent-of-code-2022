8f66aa1cf52b17411ae4f310482480aeb5b3ef2b
Llamexis <polaczek0907@gmail.com>
2022-12-10 12:49:31 +0100

    Finished Part 2

--- Advent-of-code-2022/Program.cs
+++ Advent-of-code-2022/Program.cs
@@ -1,33 +1,107 @@
-﻿var input = File.ReadAllText("input.txt")
-    .Split(' ','\r','\n').
-    Where(x=>x!="").ToArray();
+﻿using Microsoft.Win32;
+using System.Runtime.CompilerServices;
 
-int cycle = 0;
-int register = 1;
-int signalStrength = 0;
-string prevInstruction = "";
+Processor processor = new PartOneProcessor("input.txt");
+processor.action();
 
-foreach(var instruction in input)
+Console.WriteLine("Part 1: " + processor.signalStrength);
+processor = new PartTwoProcessor("input.txt");
+Console.WriteLine("Part 2:");
+processor.action();
+
+abstract class Processor
 {
-    cycle++;
-    if(cycle == 20 || cycle == 60 || cycle == 100 
-        || cycle == 140 || cycle == 180 || cycle == 220)
+    public int cycle = 0;
+    public int register = 1;
+    public int signalStrength = 0;
+    public string prevInstruction = "";
+    public string[] input;
+    
+    public Processor(string path)
     {
-        signalStrength += cycle * register;
+        input = File.ReadAllText(path)
+            .Split(' ', '\r', '\n').
+            Where(x => x != "").ToArray();
     }
-    if(instruction == "addx" && prevInstruction != "addx")
-    {
-        prevInstruction = instruction;
-    }
-    else if (instruction == "noop")
-    {
-        prevInstruction = instruction;
-    }
-    else if(prevInstruction == "addx")
-    {
-        register += int.Parse(instruction);
-        prevInstruction = instruction;
-    }
-    if (cycle == 240) break;
+
+    public abstract void action();
+    
 }
-Console.WriteLine("Part 1: " + signalStrength);
+class PartOneProcessor : Processor
+{
+    public PartOneProcessor(string path) : base(path)
+    {
+
+    }
+    public override void action()
+    {
+        foreach (var instruction in input)
+        {
+            cycle++;
+            if (cycle == 20 || cycle == 60 || cycle == 100
+                || cycle == 140 || cycle == 180 || cycle == 220)
+            {
+                signalStrength += cycle * register;
+            }
+            if (instruction == "addx" && prevInstruction != "addx")
+            {
+                prevInstruction = instruction;
+            }
+            else if (instruction == "noop")
+            {
+                prevInstruction = instruction;
+            }
+            else if (prevInstruction == "addx")
+            {
+                register += int.Parse(instruction);
+                prevInstruction = instruction;
+            }
+            if (cycle == 240) break;
+        }
+    }
+}
+class PartTwoProcessor : Processor
+{
+    public PartTwoProcessor(string path) : base(path)
+    {
+
+    }
+    public override void action()
+    {
+        int pixelPos = 0;
+        foreach (var instruction in input)
+        {
+            cycle++;
+            
+            if (pixelPos >= register - 1 && pixelPos <= register + 1)
+                Console.Write('#');
+            else
+                Console.Write('.');
+            pixelPos++;
+            if (pixelPos % 40 == 0)
+            {
+                Console.WriteLine();
+                pixelPos = 0;
+            }
+            if (cycle == 20 || cycle == 60 || cycle == 100
+                || cycle == 140 || cycle == 180 || cycle == 220)
+            {
+                signalStrength += cycle * register;
+            }
+            if (instruction == "addx" && prevInstruction != "addx")
+            {
+                prevInstruction = instruction;
+            }
+            else if (instruction == "noop")
+            {
+                prevInstruction = instruction;
+            }
+            else if (prevInstruction == "addx")
+            {
+                register += int.Parse(instruction);
+                prevInstruction = instruction;
+            }
+            
+        }
+    }
+}
